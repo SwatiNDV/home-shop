47e0ecb837915a83f68ac036280a507b05cac8d9
NAROLIYASWATI <blrhyd0@gmail.com>
2018-07-28 18:29:57 +0530

    adding files

---
+++ src/app/product-list/product-list.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductListRoutingModule } from './product-list-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ProductListRoutingModule
+  ],
+  declarations: []
+})
+export class ProductListModule { 
+
+  public name="swati";
+  
+}
